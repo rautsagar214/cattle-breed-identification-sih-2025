2b2500dfc4c150b2e278efa37415e3c9366161ef
ayush-aspivision <ayush.aspivision@gmail.com>
2025-12-03 13:20:17 +0530

    new sms route testing

--- Server/src/controllers/authController.js
+++ Server/src/controllers/authController.js
@@ -376,9 +376,10 @@ const sendOtp = async (req, res) => {
     if (process.env.FAST2SMS_API_KEY) {
       try {
         const response = await axios.post('https://www.fast2sms.com/dev/bulkV2', {
-          "route": "otp",
-          "variable_values": '',
-          "schedule_time": '',
+          "route": "q",
+          "message": message,
+          "language": "english",
+          "flash": 0,
           "numbers": phone,
         }, {
           headers: {
